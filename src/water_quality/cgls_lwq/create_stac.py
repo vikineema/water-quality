0413f688d3bde90a98d56607fbd776e6eea192a9
Victoria Neema <victoria@kartoza.com>
2025-04-17 01:47:16 +0300

    Minor fix

--- src/water_quality/cgls_lwq/create_stac.py
+++ src/water_quality/cgls_lwq/create_stac.py
@@ -1,7 +1,7 @@
 """
 Create per dataset metadata (stac files) for the Copernicus
 Land Monitoring Service (CLMS) Lake Water Quality
-2002-2012 (raster 300 m), global, 10-daily – version 1.3
+2002-2012 (raster 300 m), global, 10-daily – version 1
 product
 """
 
