26c05f5b23e18d8c5967ff15a3c9a30bdd689c27
vikineema <vikinyaga@gmail.com>
2025-04-22 21:30:07 +0300

    Add africa extent url

--- src/water_quality/cgls_lwq/constants.py
+++ src/water_quality/cgls_lwq/constants.py
@@ -3,6 +3,8 @@ Manifest files and measurements of interest for the
 Copernicus Global Land Service - Lake Water Quality ODC products
 """
 
+AFRICA_EXTENT_URL = "https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/africa-extent-bbox.json"
+
 MANIFEST_FILE_URLS = {
     "cgls_lwq300_2002_2012": "https://globalland.vito.be/download/manifest/lwq_300m_v1_10daily-reproc_netcdf/manifest_clms_global_lwq_300m_v1_10daily-reproc_netcdf_latest.txt"
 }
