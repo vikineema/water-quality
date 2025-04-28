3ce03380f0da7422c952942ff14d07a5fc7f1887
vikineema <vikinyaga@gmail.com>
2025-04-28 22:06:54 +0300

    Add products and manifest files

--- src/water_quality/cgls_lwq/constants.py
+++ src/water_quality/cgls_lwq/constants.py
@@ -7,7 +7,11 @@ AFRICA_BBOX = [-26.36, 38.35, 64.50, -47.97]
 AFRICA_EXTENT_URL = "https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/africa-extent-bbox.json"
 
 MANIFEST_FILE_URLS = {
-    "cgls_lwq300_2002_2012": "https://globalland.vito.be/download/manifest/lwq_300m_v1_10daily-reproc_netcdf/manifest_clms_global_lwq_300m_v1_10daily-reproc_netcdf_latest.txt"
+    "cgls_lwq300_2002_2012": "https://globalland.vito.be/download/manifest/lwq_300m_v1_10daily-reproc_netcdf/manifest_clms_global_lwq_300m_v1_10daily-reproc_netcdf_latest.txt",
+    "cgls_lwq300_2016_2024": "https://globalland.vito.be/download/manifest/lwq_300m_v1_10daily-nrt_netcdf/manifest_clms_global_lwq_300m_v1_10daily-nrt_netcdf_latest.txt",
+    "cgls_lwq100_2019_2024": "https://globalland.vito.be/download/manifest/lwq_100m_v1_10daily-nrt_netcdf/manifest_clms_global_lwq_100m_v1_10daily-nrt_netcdf_latest.txt",
+    "cgls_lwq300_2024_nrt": "https://globalland.vito.be/download/manifest/lwq_300m_v2_10daily-nrt_netcdf/manifest_clms_global_lwq_300m_v2_10daily-nrt_netcdf_latest.txt",
+    "cgls_lwq100_2024_nrt": "https://globalland.vito.be/download/manifest/lwq_100m_v2_10daily-nrt_netcdf/manifest_clms_global_lwq_100m_v2_10daily-nrt_netcdf_latest.txt",
 }
 MEASUREMENTS = [
     "num_obs",
