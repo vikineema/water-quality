b5175228ae49a55204991a38b2d5505ccfb7aa92
Victoria Neema <victoria@kartoza.com>
2025-04-28 23:32:07 +0300

    Update product measurements

--- src/water_quality/cgls_lwq/constants.py
+++ src/water_quality/cgls_lwq/constants.py
@@ -24,5 +24,43 @@ MEASUREMENTS_1 = [
     "turbidity_mean",
     "turbidity_sigma",
 ]
-MEASUREMENTS = {"cgls_lwq300_2002_2012": MEASUREMENTS_1, "cgls_lwq300_2016_2024": MEASUREMENTS_1}
+MEASUREMENTS_2 = [
+    "first_obs",
+    "last_obs",
+    "num_obs",
+    "trophic_state_mean",
+    "turbidity_blended_mean",
+]
+MEASUREMENTS_3 = [
+    "chla_mean",
+    "chla_uncertainty",
+    "first_obs",
+    "floating_cyanobacteria",
+    "last_obs",
+    "num_obs",
+    "quality_flags",
+    "trophic_state_index",
+    "tsm_mean",
+    "tsm_uncertainty",
+    "turbidity_mean",
+]
+MEASUREMENTS_4 = [
+    "chla_mean",
+    "first_obs",
+    "floating_cyanobacteria",
+    "last_obs",
+    "num_obs",
+    "quality_flags",
+    "trophic_state_index",
+    "tsm_mean",
+    "turbidity_mean",
+]
+
+MEASUREMENTS = {
+    "cgls_lwq300_2002_2012": MEASUREMENTS_1,
+    "cgls_lwq300_2016_2024": MEASUREMENTS_1,
+    "cgls_lwq100_2019_2024": MEASUREMENTS_2,
+    "cgls_lwq300_2024_nrt": MEASUREMENTS_3,
+    "cgls_lwq100_2024_nrt": MEASUREMENTS_4,
+}
 NAMING_PREFIX = "c_gls"
